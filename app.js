c5a1a5f8b093dd6c358ef32f0ad07f9b5e8ab12a
Alexander Zakharov <alex@Alexs-MacBook-Pro.local>
2018-07-22 13:49:37 -0400

    Test 7

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var locale = require('locale');
 var supported = new locale.Locales(["en", "ru", "uk"]);
 
-var server = http.createServer(function(request, response) {
-    var locales = new locale.Locales(request.headers["accept-language"]);
-    response.writeHead(200, {"Content-Type": "text/html"});
+app.get('/', function (req, res) {
+    var locales = new locale.Locales(req.headers["accept-language"]);
     var bestLocale = locales.best(supported);
 
+    res.header("Content-Type", "text/html");
+
+    var text_service_status;
+
     if (bestLocale == "ru") {
-        response.end("Привет мир! Ваш язык: " + bestLocale);
+        text_service_status = '<pre><strong>Статус:</strong><br />&#9;Все сервисы <font color="green"><strong>ОНЛАЙН</strong></font> и работают без неполадок.<br /></br /></br /></br />';
     }
     else if (bestLocale == "uk") {
-        response.end("Прывит мыру! Ваша мова: " + bestLocale);
+        text_service_status = '<pre><strong>Статус:</strong><br />&#9;Всі сервіси <font color="green"><strong>ОНЛАЙН</strong></font> та повністю працюють.<br /></br /></br /></br />';
     }
     else {
-        response.end("Hello World! Your language: " + bestLocale);
+        text_service_status = '<pre><strong>Status:</strong><br />&#9;All services are <font color="green"><strong>ONLINE</strong></font> and fully operational.<br /></br /></br /></br />';
     }
 
+    res.send(
+        text_service_status +
+        '<font size="2">Copyright &copy; 2018 Galactic Networks, Inc.</font></pre>'
+    );
 });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
+
+app.listen(port, function () {
+    //console.log('Example app listening on port 1337!');
+});
