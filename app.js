352ea20a6ffcff9b3bd4529abf88dbb39b8a451f
Alexander Zakharov <alex@Alexs-MacBook-Pro.local>
2018-07-22 12:15:56 -0400

    Testing the app 2

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
 });
 
-app.listen(80, function () {
-    //console.log('Example app listening on port 3000!');
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
