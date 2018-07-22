9737ae6b369984003b558eb42f353c4771ceba00
Alexander Zakharov <alex@Alexs-MacBook-Pro.local>
2018-07-22 14:22:57 -0400

    Test 8

--- app.js
+++ app.js
@@ -9,21 +9,31 @@ app.get('/', function (req, res) {
 
     res.header("Content-Type", "text/html");
 
+    var text_dns_address;
     var text_service_status;
+    var text_current_issues;
 
     if (bestLocale == "ru") {
-        text_service_status = '<pre><strong>Статус:</strong><br />&#9;Все сервисы <font color="green"><strong>ОНЛАЙН</strong></font> и работают без неполадок.<br /></br /></br /></br />';
+        text_dns_address = '<pre><strong>DNS адрес:</strong><br />&#9;137.117.231.56 (впишите как Primary и Secondary DNS)<br /></br /></br />';
+        text_service_status = '<strong>Статус:</strong><br />&#9;Все сервисы <font color="green"><strong>ОНЛАЙН</strong></font> и работают без неполадок.<br /></br /></br />';
+        text_current_issues = '<strong>Текущие проблемы:</strong><br />&#9;- Spotify в данный момент не разрешает авторизироваться. Мы работаем над этим.';
     }
     else if (bestLocale == "uk") {
-        text_service_status = '<pre><strong>Статус:</strong><br />&#9;Всі сервіси <font color="green"><strong>ОНЛАЙН</strong></font> та повністю працюють.<br /></br /></br /></br />';
+        text_dns_address = '<pre><strong>DNS адреса:</strong><br />&#9;137.117.231.56 (впишить як Primary та Secondary DNS)<br /></br /></br />';
+        text_service_status = '<strong>Статус:</strong><br />&#9;Всі сервіси <font color="green"><strong>ОНЛАЙН</strong></font> та повністю працюють.<br /></br /></br />';
+        text_current_issues = '<strong>Поточні проблеми:</strong><br />&#9;- Spotify поки що не дозволяє авторизуватися. Ми працюємо над цим.';
     }
     else {
-        text_service_status = '<pre><strong>Status:</strong><br />&#9;All services are <font color="green"><strong>ONLINE</strong></font> and fully operational.<br /></br /></br /></br />';
+        text_dns_address = '<pre><strong>DNS Address:</strong><br />&#9;137.117.231.56 (set as both Primary and Secondary DNS)<br /></br /></br />';
+        text_service_status = '<strong>Status:</strong><br />&#9;All services are <font color="green"><strong>ONLINE</strong></font> and fully operational.<br /></br /></br />';
+        text_current_issues = '<strong>Current Issues:</strong><br />&#9;- Spotify currently does not let you log in. We are working on a fix.';
     }
 
     res.send(
+        text_dns_address +
         text_service_status +
-        '<font size="2">Copyright &copy; 2018 Galactic Networks, Inc.</font></pre>'
+        text_current_issues +
+        '<br /></br /></br /></br /></br /><font size="2">Copyright &copy; 2018 Galactic Networks, Inc.</font></pre>'
     );
 });
 
